08fc1640b3f5b874f609b4180cb64ff11c8571e5
D1Scripts <vapecatog@gmail.com>
2025-03-29 20:05:35 -0500

    Add files via upload

---
+++ HeadTag/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Lamaa'
+description 'Custom Head Tags with NativeUI'
+version '1.0'
+
+client_scripts {
+    '@NativeUI/NativeUI.lua', -- Make sure NativeUI is in your resources folder!
+    'client.lua'
+}
+
+server_script 'server.lua'
